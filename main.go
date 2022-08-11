8c2b279f8a4b2d4433ea7aa7fe717828404d10f0
Naoya Yamashita <conao3@gmail.com>
2022-08-12 00:08:15 +0900

    depends go-flags

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	flags "github.com/jessevdk/go-flags"
+)
+
+type Options struct {
+	Verbose []bool `short:"v" long:"verbose" description:"Show verbose debug information"`
+}
 
 func main() {
+	var opts Options
 	fmt.Println("Hello World")
+	args, err := flags.Parse(&opts)
+	if err != nil {
+		os.Exit(1)
+	}
+	fmt.Println(args)
+	fmt.Println(opts)
 }
