828accf283b4e8c14941682ba1c1afcc910cdb38
Naoya Yamashita <conao3@gmail.com>
2022-08-12 01:24:59 +0900

    implement cat

--- main.go
+++ main.go
@@ -2,22 +2,54 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"os"
 
 	flags "github.com/jessevdk/go-flags"
 )
 
-type Options struct {
-	Verbose []bool `short:"v" long:"verbose" description:"Show verbose debug information"`
+func main_() (int, error) {
+	var opts struct {
+		Input string `short:"i" long:"input" description:"Input file" default:"-"`
+	}
+
+	args, err := flags.Parse(&opts)
+	if err != nil {
+		return 1, err
+	}
+
+	if len(args) > 0 {
+		return 1, fmt.Errorf("unexpected argument: %q", args[0])
+	}
+
+	var r io.Reader
+	switch opts.Input {
+	case "", "-":
+		r = os.Stdin
+	default:
+		f, err := os.Open(opts.Input)
+		if err != nil {
+			return 1, err
+		}
+		defer f.Close()
+		r = f
+	}
+
+	t, err := io.ReadAll(r)
+	if err != nil {
+		return 1, err
+	}
+
+	fmt.Print(string(t))
+
+	return 1, nil
 }
 
 func main() {
-	var opts Options
-	fmt.Println("Hello World")
-	args, err := flags.Parse(&opts)
+	ret, err := main_()
 	if err != nil {
-		os.Exit(1)
+		log.Println(err)
+		os.Exit(ret)
 	}
-	fmt.Println(args)
-	fmt.Println(opts)
 }
